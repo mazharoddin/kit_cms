e0968125cec8b0236a3a8c546cf96cc28e3fae92
dadamsuk <da@dsc.net>
2013-01-22 21:14:43 +0000

    fix

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,8 +3,10 @@ class Activity < ActiveRecord::Base
   belongs_to :user
 
   attr_accessible :system_id, :name, :user_id, :category, :description
-  def self.add(sys_id, name, user_id=0, category='', description='')
-    Activity.create(:user_id=>user_id, :system_id=>sys_id, :name=>name, :category=>category, :description=>description).save
+
+
+  def self.add(sys_id, name, user_or_user_id=0, category='', description='')
+    Activity.create(:user_id=>user_or_user_id.is_a?(Integer) ? user_or_user_id : user_or_user_id.id, :system_id=>sys_id, :name=>name, :category=>category, :description=>description).save
   end
 
 end
